bd2144dd6aedb310e8c802819c661ca528b2e5ce
Ryna Truong <rynatruong@outlook.com>
2020-07-15 17:31:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,12 +14,7 @@ class Song
   end
   
   def artist_name
-    if artist 
-      return 
-      self.artist.name
-    else
-      return nil
-    end
+    self.artist.name
     # binding.pry
   end
 end
